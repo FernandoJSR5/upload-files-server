eb8b49a2c85c4d967ccff6d500c908220af28523
FernandoJSR5 <fernando.jsr5@gmail.com>
2020-12-10 10:30:32 -0300

    commit 2

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
-const express = require('express')
-const server = express()
-const cors = require('cors')
-const upload = require('./upload')
+const express = require('express');
+const bodyParser = require('body-parser');
+const router = express.Router();
+const cors = require('cors');
+const server = express();
 
 var corsOptions = {
   origin: '*',
   optionsSuccessStatus: 200,
 }
 
-server.use(cors(corsOptions))
+server.use(cors(corsOptions));
+server.use(bodyParser.json({limit: '50mb'}));
+server.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
-server.post('/upload', upload)
+router.post('/upload', (request, response) => {
+  console.log(request.body);
+  response.json();
+})
+
+server.use("/", router);
 
 server.listen(8000, () => {
   console.log('Server started!')
